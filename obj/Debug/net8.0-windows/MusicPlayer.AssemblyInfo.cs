321c51ab0ea61e5d0d392b37516a94c9db99150e
Marcin Ryba <marcinroffice@gmail.com>
2025-10-16 22:09:41 +0200

    fix queue

--- obj/Debug/net8.0-windows/MusicPlayer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MusicPlayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fde591692e173707134fc68471fbc5d9312d5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ab6cf08027d31320fc86b5f847b602b1884abe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
